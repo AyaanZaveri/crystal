c4bed640a1a38f05b633cc66d052c622986c5081
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-04-24 12:44:14 -0400

    CORS

--- utils/getSearch.ts
+++ utils/getSearch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getSearch = async (query: string): Promise<any> => {
-  const url = `https://api.coingecko.com/api/v3/search?query=${query}`
+  const url = `https://cors-anywhere-production-c185.up.railway.app/https://api.coingecko.com/api/v3/search?query=${query}`
   const res = await axios.get(url)
   return res.data
 }
