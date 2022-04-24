0070e8766e6d6e82c73383ef4984abe09d696796
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-04-23 21:33:19 -0400

    Eth Gas

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { getEthGas } from '../../utils/getEthGas'
 import { getSearch } from '../../utils/getSearch'
 
 const Navbar = () => {
   const [input, setInput] = useState('')
   const [searchRes, setSearchRes] = useState<any>([])
+  const [ethGas, setEthGas] = useState<string>('')
 
   useEffect(() => {
     getSearch(input).then((res: any) => {
@@ -13,48 +15,57 @@ const Navbar = () => {
 
   console.log(searchRes)
 
+  useEffect(() => {
+    getEthGas().then((res) => setEthGas(res))
+  }, [])
+
   return (
-    <div className="h-20 w-full border-b border-slate-600 font-['Inter']">
-      <div className="absolute left-0 -z-10 h-24 w-24 rounded-full bg-sky-500 blur-3xl"></div>
-      <div className="absolute right-0 -z-10 h-16 w-16 rounded-full bg-purple-500 blur-2xl"></div>
+    <div className="flex flex-col">
+      <div className="flex h-8 w-full items-center border-b border-slate-600 font-['Inter']">
+        <span className="ml-8 font-mono font-medium text-white text-sm">{ethGas}</span>
+      </div>
+      <div className="h-20 w-full border-b border-slate-600 font-['Inter']">
+        <div className="absolute left-0 -z-10 h-24 w-24 rounded-full bg-sky-500 blur-3xl"></div>
+        <div className="absolute right-0 -z-10 h-16 w-16 rounded-full bg-purple-500 blur-2xl"></div>
 
-      <div className="flex h-full w-full items-center justify-between px-8">
-        <h1 className="font-mono text-2xl text-white">Crystal</h1>
-        <div>
-          <input
-            placeholder="Search"
-            onChange={(e) => setInput(e.target.value)}
-            className="w-72 rounded-md bg-slate-800 py-2 px-4 text-sm text-white transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-1 focus:ring-offset-sky-700 active:bg-slate-900"
-          />
-          {searchRes.length > 0 && input.length > 0 ? (
-            <ul className="absolute mt-3 w-72 overflow-hidden rounded-lg bg-slate-900/80 shadow-xl backdrop-blur">
-              {searchRes?.map((res: any) => (
-                <li
-                  className="flex cursor-pointer items-center overflow-hidden p-3 text-white transition duration-300 ease-in-out hover:bg-sky-500/5"
-                  onClick={() =>
-                    (location.href = `/crypto/${res?.name.toLowerCase()}`)
-                  }
-                >
-                  <div className="inline-flex items-center gap-2">
-                    <img
-                      className="w-5 rounded-full"
-                      src={
-                        res?.thumb
-                          ? res?.thumb
-                          : 'https://via.placeholder.com/150'
-                      }
-                      alt=""
-                    />
-                    <span>{res?.name}</span>
+        <div className="flex h-full w-full items-center justify-between px-8">
+          <h1 className="font-mono text-2xl text-white">Crystal</h1>
+          <div>
+            <input
+              placeholder="Search"
+              onChange={(e) => setInput(e.target.value)}
+              className="w-72 rounded-md bg-slate-800 py-2 px-4 text-sm text-white transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-1 focus:ring-offset-sky-700 active:bg-slate-900"
+            />
+            {searchRes.length > 0 && input.length > 0 ? (
+              <ul className="absolute mt-3 w-72 overflow-hidden rounded-lg bg-slate-900/80 shadow-xl backdrop-blur">
+                {searchRes?.map((res: any) => (
+                  <li
+                    className="flex cursor-pointer items-center overflow-hidden p-3 text-white transition duration-300 ease-in-out hover:bg-sky-500/5"
+                    onClick={() =>
+                      (location.href = `/crypto/${res?.name.toLowerCase()}`)
+                    }
+                  >
+                    <div className="inline-flex items-center gap-2">
+                      <img
+                        className="w-5 rounded-full"
+                        src={
+                          res?.thumb
+                            ? res?.thumb
+                            : 'https://via.placeholder.com/150'
+                        }
+                        alt=""
+                      />
+                      <span>{res?.name}</span>
 
-                    <span className="h-min cursor-default rounded-sm bg-slate-800 px-1.5 text-sm text-slate-400 duration-300 hover:bg-sky-500 hover:text-white">
-                      {res?.symbol?.toUpperCase()}
-                    </span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          ) : null}
+                      <span className="h-min cursor-default rounded-sm bg-slate-800 px-1.5 text-sm text-slate-400 duration-300 hover:bg-sky-500 hover:text-white">
+                        {res?.symbol?.toUpperCase()}
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
--- utils/getEthGas.ts
+++ utils/getEthGas.ts
@@ -1,12 +1,11 @@
 import axios from 'axios'
 const cheerio = require('cheerio')
 
-export const getMoonPhase = async (): Promise<string> => {
-  const { data } = await axios.get('https://www.coingecko.com/')
-  const base = 'https://nineplanets.org'
-  const $ = cheerio.load(data)
-  const moonPhase = $(
-    'body > main > section.section.has-background-info-light > div > div > div.column.has-text-centered > div > img'
-  ).attr('src')
-  return base + moonPhase
+export const getEthGas = async (): Promise<string> => {
+  const url = 'https://ethgasstation.info/json/ethgasAPI.json'
+  const res = await axios.get(url)
+  const data = res.data
+  const { fast, safeLow } = data
+  const gasPrice = fast ? String(fast / 10) : String(safeLow / 10)
+  return '⛽️ ' + gasPrice + ' GWEI'
 }
